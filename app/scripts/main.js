b665ef993b85578cc4df3353b9a98b3f43e5248d
karlos g liberal <karlosgliberal@gmail.com>
2014-04-07 13:52:13 +0200

    Probando en el paso de las secciones, se le tiene que meter mas mano a, jquery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
-  $('.carta-item').waypoint(function(direction) {
-    $('#movida').removeClass('circulo');
-        console.log('hola');
-    $('#movida').addClass('circulo-efecto');
-  }); 
-  $('.dos').waypoint(function(direction) {
-    $('.lateral > div').removeClass('circulo-efecto');
-    $('.dos').addClass('circuloEfecto');
-  }); 
+
+  $('#principal').waypoint(function(direction) {
+    $('#carta').removeClass('circulo-efecto');
+    $('#carta').addClass('circulo');
+  });
+
+  $('#carta-gerente').waypoint(function(direction) {
+    $('#carta').removeClass('circulo');
+    $('#carta').addClass('circulo-efecto');
+  });
+
+  $('#principales-logros').waypoint(function(direction) {
+    $('.circulo-efecto').removeClass('.circulo-efecto');
+    $('#logros').removeClass('circulo');
+    $('#logros').addClass('circulo-efecto');
+  });
+
   $('.tres').waypoint(function(direction) {
     $('.lateral > div').removeClass('circuloEfecto');
     $('.tres').addClass('circuloEfecto');
