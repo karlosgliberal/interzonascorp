086c1131bb2c147732ed7d3e5e93fdc50ac3eefa
karlos g liberal <karlosgliberal@gmail.com>
2014-04-07 13:22:15 +0200

    Primer prototipo del efecto.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   $('.carta-item').waypoint(function(direction) {
-    // $('#movida').removeClass('circulo');
-    $('#movida').addClass('efecto');
+    $('#movida').removeClass('circulo');
+        console.log('hola');
+    $('#movida').addClass('circulo-efecto');
   }); 
   $('.dos').waypoint(function(direction) {
-    $('.lateral > div').removeClass('circuloEfecto');
+    $('.lateral > div').removeClass('circulo-efecto');
     $('.dos').addClass('circuloEfecto');
   }); 
   $('.tres').waypoint(function(direction) {
