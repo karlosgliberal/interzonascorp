d240fb9716bbece19058a7c0ae91b392ac28223a
karlos g liberal <karlosgliberal@gmail.com>
2014-04-08 10:20:39 +0200

    Una primera versión para mejorar, pero ya hace lo que tiene que hacer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,39 @@
 $(document).ready(function(){
+  var borrarClase = function(element){
+    console.log('movida');
+    // $(element).removeClass('circulo');
+    // $(element).addClass('circulo-efecto');
+    $('.circulo-efecto').each(function(index, valor){
+      $(valor).removeClass('circulo-efecto');
+      $(valor).addClass('circulo');
+    });
+  }
 
   $('#principal').waypoint(function(direction) {
-    $('#carta').removeClass('circulo-efecto');
-    $('#carta').addClass('circulo');
+    borrarClase();
+    // $('#carta').removeClass('circulo-efecto');
+    // $('#carta').addClass('circulo');
   });
 
   $('#carta-gerente').waypoint(function(direction) {
+    borrarClase();
     $('#carta').removeClass('circulo');
     $('#carta').addClass('circulo-efecto');
   });
 
   $('#principales-logros').waypoint(function(direction) {
-    $('.circulo-efecto').removeClass('.circulo-efecto');
+    borrarClase();
     $('#logros').removeClass('circulo');
     $('#logros').addClass('circulo-efecto');
   });
 
-  $('.tres').waypoint(function(direction) {
-    $('.lateral > div').removeClass('circuloEfecto');
-    $('.tres').addClass('circuloEfecto');
-  }); 
-  $('.cuatro').waypoint(function(direction) {
-    $('.lateral > div').removeClass('circuloEfecto');
-    $('.cuatro').addClass('circuloEfecto');
-  }); 
+  // $('.tres').waypoint(function(direction) {
+  //   $('.lateral > div').removeClass('circuloEfecto');
+  //   $('.tres').addClass('circuloEfecto');
+  // }); 
+  // $('.cuatro').waypoint(function(direction) {
+  //   $('.lateral > div').removeClass('circuloEfecto');
+  //   $('.cuatro').addClass('circuloEfecto');
+  // }); 
+
 });
