2bd05f3f95a37092c3649fa8e5c7ae18c9fb4d71
karlos g liberal <karlosgliberal@gmail.com>
2014-04-08 11:36:27 +0200

    Preparando el resto de secciones para el menú de bolinas

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,32 +1,80 @@
 $(document).ready(function(){
-  var borrarClase = function(){
+
+  var borrarEfecto = function(){
     $('.circulo-efecto').each(function(index, valor){
       $(valor).removeClass('circulo-efecto');
       $(valor).addClass('circulo');
     });
+    $('.circulo-efecto-peque').each(function(index, valor){
+      $(valor).removeClass('circulo-efecto-peque');
+      $(valor).addClass('circulo-peque');
+    });
   }
 
   $('#principal').waypoint(function(direction) {
-    borrarClase();
+    borrarEfecto();
   },{ offset: '-400'});
 
   $('#carta-gerente').waypoint(function(direction) {
-    borrarClase();
+    borrarEfecto();
     $('#carta').removeClass('circulo');
     $('#carta').addClass('circulo-efecto');
-  });
+  }, {offset: '40%'});
 
   $('#principales-logros').waypoint(function(direction) {
-    borrarClase();
+    borrarEfecto();
     $('#logros').removeClass('circulo');
     $('#logros').addClass('circulo-efecto');
   });
 
   $('#valor-bienestar').waypoint(function(direction) {
-    borrarClase();
+    borrarEfecto();
     $('#valor').removeClass('circulo');
     $('#valor').addClass('circulo-efecto');
   });
+
+  $('#radio-saludable').waypoint(function(direction) {
+    borrarEfecto();
+    $('#consejo').removeClass('circulo-peque');
+    $('#consejo').addClass('circulo-efecto-peque');
+  });
+
+  $('#proyecto-bdp').waypoint(function(direction) {
+    borrarEfecto();
+    $('#bdp').removeClass('circulo-peque');
+    $('#bdp').addClass('circulo-efecto-peque');
+  });
+
+  $('#proyecto-bdp').waypoint(function(direction) {
+    borrarEfecto();
+    $('#bdp').removeClass('circulo-peque');
+    $('#bdp').addClass('circulo-efecto-peque');
+  });
+
+  $('#blanco-rojo').waypoint(function(direction) {
+    borrarEfecto();
+    $('#blanco').removeClass('circulo-peque');
+    $('#blanco').addClass('circulo-efecto-peque');
+  });
+
+  $('#brigada-emprendedores').waypoint(function(direction) {
+    borrarEfecto();
+    $('#brigada').removeClass('circulo-peque');
+    $('#brigada').addClass('circulo-efecto-peque');
+  });
+
+  $('#planta-efluentes').waypoint(function(direction) {
+    borrarEfecto();
+    $('#planta').removeClass('circulo-peque');
+    $('#planta').addClass('circulo-efecto-peque');
+  });
+
+  $('#indicadores-gri').waypoint(function(direction) {
+    borrarEfecto();
+    $('#gri').removeClass('circulo');
+    $('#gri').addClass('circulo-efecto');
+  });
+
 
   // $('.tres').waypoint(function(direction) {
   //   $('.lateral > div').removeClass('circuloEfecto');
