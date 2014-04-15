0dd75012805665ba9b32144e215d435a15182d2f
karlos g liberal <karlosgliberal@gmail.com>
2014-04-15 12:15:13 +0200

    Bug corregido

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -42,7 +42,6 @@ $(document).ready(function(){
   }
 
   var sistemaWaypoint = function(id, elemento, peque){
-        console.log('name');
     elemento.waypoint(function(direction) {
       if (direction === 'down') {
         borrarEfecto();
@@ -195,62 +194,5 @@ $(document).ready(function () {
     });
 });
 
-  // $('#principales-logros').waypoint(function(direction) {
-  //   borrarEfecto();
-  //   $('#logros').removeClass('circulo');
-  //   $('#logros').addClass('circulo-efecto');
-  // });
-
-  // $('#valor-bienestar').waypoint(function(direction) {
-  //   borrarEfecto();
-  //   $('#valor').removeClass('circulo');
-  //   $('#valor').addClass('circulo-efecto');
-  // });
-
-  // $('#radio-saludable').waypoint(function(direction) {
-  //   borrarEfecto();
-  //   $('#consejo').removeClass('circulo-peque');
-  //   $('#consejo').addClass('circulo-efecto-peque');
-  // });
-
-  // $('#proyecto-bdp').waypoint(function(direction) {
-  //   borrarEfecto();
-  //   $('#bdp').removeClass('circulo-peque');
-  //   $('#bdp').addClass('circulo-efecto-peque');
-  // });
-
-  // $('#blanco-rojo').waypoint(function(direction) {
-  //   borrarEfecto();
-  //   $('#blanco').removeClass('circulo-peque');
-  //   $('#blanco').addClass('circulo-efecto-peque');
-  // });
-
-  // $('#brigada-emprendedores').waypoint(function(direction) {
-  //   borrarEfecto();
-  //   $('#brigada').removeClass('circulo-peque');
-  //   $('#brigada').addClass('circulo-efecto-peque');
-  // });
-
-  // $('#planta-efluentes').waypoint(function(direction) {
-  //   borrarEfecto();
-  //   $('#planta').removeClass('circulo-peque');
-  //   $('#planta').addClass('circulo-efecto-peque');
-  // });
-
-  // $('#indicadores-gri').waypoint(function(direction) {
-  //   borrarEfecto();
-  //   $('#gri').removeClass('circulo');
-  //   $('#gri').addClass('circulo-efecto');
-  // });
-
-
-  // $('.tres').waypoint(function(direction) {
-  //   $('.lateral > div').removeClass('circuloEfecto');
-  //   $('.tres').addClass('circuloEfecto');
-  // }); 
-  // $('.cuatro').waypoint(function(direction) {
-  //   $('.lateral > div').removeClass('circuloEfecto');
-  //   $('.cuatro').addClass('circuloEfecto');
-  // }); 
 
 });
